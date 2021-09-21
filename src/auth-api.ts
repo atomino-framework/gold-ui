467fe374510c3046156e0d7aea263ebc1861485a
Gergely Laborci <gergely@laborci.hu>
2021-09-21 12:23:35 +0200

    auth api updated

--- src/auth-api.ts
+++ src/auth-api.ts
@@ -4,14 +4,19 @@ import user from "./user";
 
 export default class AuthApi implements IAuthApi {
 
-	constructor(private apibase: string = "/gold/auth") {}
+	constructor(private apibase: string = "/gold/auth", private onLogin: Function | null = null) {}
 
 	get(): Promise<any> {
-		return fetch(this.apibase + '/get', {method: "POST"}).then(handleFetch).then(res => user.set(res));
+		return fetch(this.apibase + '/get', {method: "POST"}).then(handleFetch).then(res => {
+			user.set(res);
+			return res;
+		});
 	}
 
 	login(login: string, password: string): Promise<any> {
-		return fetch(this.apibase + '/login', {method: "POST", body: JSON.stringify({login, password})}).then(handleFetch).then(res => this.get());
+		return fetch(this.apibase + '/login', {method: "POST", body: JSON.stringify({login, password})}).then(handleFetch).then(res => {
+			if (null !== this.get() && this.onLogin !== null) this.onLogin();
+		});
 	}
 
 	logout(): Promise<any> {

aa19571a1616c4a3fc49392cfff66a891f34dff2
Gergely Laborci <gergely@laborci.hu>
2021-09-21 12:29:25 +0200

    auth

--- lib/auth-api.d.ts
+++ lib/auth-api.d.ts
@@ -1,7 +1,8 @@
 import type { IAuthApi } from "./interfaces";
 export default class AuthApi implements IAuthApi {
     private apibase;
-    constructor(apibase?: string);
+    private onLogin;
+    constructor(apibase?: string, onLogin?: Function | null);
     get(): Promise<any>;
     login(login: string, password: string): Promise<any>;
     logout(): Promise<any>;
